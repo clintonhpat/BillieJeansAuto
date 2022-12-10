29923ec255db9fd51ad380e40e0287b74326912a
clintonhpat <118099686+clintonhpat@users.noreply.github.com>
2022-12-10 04:34:41 -0600

    Update EditUser.js

--- src/features/users/EditUser.js
+++ src/features/users/EditUser.js
@@ -5,7 +5,7 @@ import PulseLoader from 'react-spinners/PulseLoader'
 import useTitle from '../../hooks/useTitle'
 
 const EditUser = () => {
-    useTitle('techNotes: Edit User')
+    useTitle('Shop Notes: Edit User')
 
     const { id } = useParams()
 
